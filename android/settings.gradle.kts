a1177b8963e103d07cb662adead085571a6e95e9
kirasimon565 <kirasimon136@gmail.com>
2026-01-10 17:33:23 +0200

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,12 +1,16 @@
+import java.util.Properties
+
 pluginManagement {
-    val flutterSdkPath =
-        run {
-            val properties = java.util.Properties()
-            file("local.properties").inputStream().use { properties.load(it) }
-            val flutterSdkPath = properties.getProperty("flutter.sdk")
-            require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
-            flutterSdkPath
+    val flutterSdkPath = run {
+        val properties = Properties()
+        val localPropertiesFile = settingsDir.resolve("local.properties")
+        if (localPropertiesFile.exists()) {
+            localPropertiesFile.inputStream().use { properties.load(it) }
         }
+        val path = properties.getProperty("flutter.sdk")
+        requireNotNull(path) { "flutter.sdk not set in local.properties" }
+        path
+    }
 
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
@@ -18,9 +22,21 @@ pluginManagement {
 }
 
 plugins {
-    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.11.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
+    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
 }
 
 include(":app")
+
+// Plugin loading logic for Flutter
+val flutterProjectRoot = settingsDir.parentFile.toPath()
+val pluginsProperties = Properties()
+val pluginsFile = File(flutterProjectRoot.toFile(), ".flutter-plugins")
+if (pluginsFile.exists()) {
+    pluginsFile.inputStream().use { pluginsProperties.load(it) }
+}
+
+pluginsProperties.forEach { name, path ->
+    val pluginDirectory = flutterProjectRoot.resolve(path.toString()).resolve("android").toFile()
+    include(":$name")
+    project(":$name").projectDir = pluginDirectory
+}
