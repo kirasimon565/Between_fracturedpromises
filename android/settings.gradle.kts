c1aa179cf627cce4594235f7d1a6564faedc9cc6
kirasimon565 <kirasimon136@gmail.com>
2026-01-12 22:22:43 +0200

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -3,8 +3,7 @@ import java.io.File
 
 pluginManagement {
     val flutterSdkPath = run {
-        // Using the full path to the class to bypass "Unresolved Reference"
-        val properties = java.util.Properties() 
+        val properties = Properties()
         val localPropertiesFile = settingsDir.resolve("local.properties")
         if (localPropertiesFile.exists()) {
             localPropertiesFile.inputStream().use { properties.load(it) }
@@ -25,24 +24,9 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
-    id("com.android.application") version "8.1.0" apply false
+    id("com.android.application") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.8.22" apply false
     id("com.google.gms.google-services") version "4.3.15" apply false
 }
 
 include(":app")
-
-// Fully qualified names here as well for safety
-val flutterProjectRoot = settingsDir.parentFile.toPath()
-val pluginsFile = java.io.File(flutterProjectRoot.toFile(), ".flutter-plugins")
-if (pluginsFile.exists()) {
-    val pluginsProperties = java.util.Properties()
-    pluginsFile.inputStream().use { pluginsProperties.load(it) }
-    pluginsProperties.forEach { name, path ->
-        val pluginDirectory = flutterProjectRoot.resolve(path.toString()).resolve("android").toFile()
-        if (pluginDirectory.exists()) {
-            include(":$name")
-            project(":$name").projectDir = pluginDirectory
-        }
-    }
-}
