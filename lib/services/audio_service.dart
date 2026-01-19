60d62f5aa707842ff70f803fc6424951d226c253
kirasimon565 <kirasimon136@gmail.com>
2026-01-19 19:30:20 +0200

    Update audio_service.dart

--- lib/services/audio_service.dart
+++ lib/services/audio_service.dart
@@ -8,8 +8,13 @@ class AudioService extends GetxService {
   @override
   void onInit() {
     super.onInit();
-    _musicPlayer.setReleaseMode(ReleaseMode.loop); // Keep the noir themes looping
+    // Keep the noir themes looping for atmosphere
+    _musicPlayer.setReleaseMode(ReleaseMode.loop); 
   }
+
+  // --- NEW: GENERIC THEME METHOD ---
+  // This fixes the 'playTheme' error in your WelcomeScreen
+  void playTheme(String trackName) => _playMusic(trackName);
 
   // --- MUSIC METHODS ---
   void playIntroTheme() => _playMusic('theme_intro.mp3');
@@ -17,6 +22,7 @@ class AudioService extends GetxService {
   void playTragedyTheme() => _playMusic('theme_tragedy.mp3');
 
   // --- FX METHODS ---
+  // Maps to assets/fx/ as defined in your pubspec
   void playShatter() => _playSfx('glass_shatter.mp3');
   void playPing() => _playSfx('msg_ping.mp3');
   void playSend() => _playSfx('msg_send.mp3');
@@ -25,16 +31,33 @@ class AudioService extends GetxService {
 
   // Internal Helpers
   Future<void> _playMusic(String track) async {
-    await _musicPlayer.stop();
-    await _musicPlayer.play(AssetSource('music/$track'));
+    try {
+      await _musicPlayer.stop();
+      // Plays from assets/music/
+      await _musicPlayer.play(AssetSource('music/$track'));
+    } catch (e) {
+      print("Error playing music: $e");
+    }
   }
 
   Future<void> _playSfx(String sfx) async {
-    await _fxPlayer.play(AssetSource('fx/$sfx'));
+    try {
+      // Plays from assets/fx/
+      await _fxPlayer.play(AssetSource('fx/$sfx'));
+    } catch (e) {
+      print("Error playing sfx: $e");
+    }
   }
 
   void stopAll() {
     _musicPlayer.stop();
     _fxPlayer.stop();
+  }
+
+  @override
+  void onClose() {
+    _musicPlayer.dispose();
+    _fxPlayer.dispose();
+    super.onClose();
   }
 }
