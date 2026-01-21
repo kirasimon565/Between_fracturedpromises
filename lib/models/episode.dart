f0fc98941fada4aa3c05de6b8bc9e1ef64344d7a
kirasimon565 <kirasimon136@gmail.com>
2026-01-21 20:04:00 +0200

    Update episode.dart

--- lib/models/episode.dart
+++ lib/models/episode.dart
@@ -5,21 +5,31 @@ class Episode {
   final String title;
   final List<Scene> scenes;
 
-  Episode({required this.id, required this.title, required this.scenes});
+  Episode({
+    required this.id,
+    required this.title,
+    required this.scenes,
+  });
 
   factory Episode.fromJson(Map<String, dynamic> json) {
     return Episode(
       id: json['id']?.toString() ?? '',
+
       title: json['title']?.toString() ?? 'Untitled Episode',
+
       scenes: (json['scenes'] as List?)
-              ?.map((e) => Scene.fromJson(e as Map<String, dynamic>))
-              .toList() ?? [],
+              ?.whereType<Map<String, dynamic>>()
+              .map((e) => Scene.fromJson(e))
+              .toList() ??
+          const [],
     );
   }
 
-  Map<String, dynamic> toJson() => {
-    'id': id,
-    'title': title,
-    'scenes': scenes.map((e) => e.toJson()).toList(),
-  };
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'scenes': scenes.map((e) => e.toJson()).toList(),
+    };
+  }
 }
