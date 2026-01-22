4cb83f43c3116ad2f4788c3640cbce5cdb391350
kirasimon565 <kirasimon136@gmail.com>
2026-01-22 20:50:26 +0200

    Update splash_screen.dart

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -3,7 +3,7 @@ import 'package:get/get.dart';
 import '../../app/routes.dart';
 import '../../app/constants.dart';
 import '../../services/auth_service.dart';
-import 'between_fractured_promises/services/state_service.dart';
+import 'between_fractured_promises/lib/services/state_service.dart';
 // 🛠️ StoryRuntime is the new brain in the logic folder
 import '../../logic/story_runtime.dart'; 
 import '../../services/audio_service.dart'; 
